75e0a9fe0360f8d911b502544f45bc2e74e380e1
w3dd1e <kelly.weddle@gmail.com>
2025-01-17 22:39:21 -0600

    Outline project goals/features

---
+++ rps.js
@@ -0,0 +1,11 @@
+/*
+
+Computer randomly selects rock, paper, or scissors.
+Ask the user for a selection.
+Display score.
+Following traditional rules, if the user input beats the computer, add one to win total.
+Else, add 1 to loss total.
+Repeat 4 more times for a total of 5 rounds.
+At the end of 5 rounds, declare a winner.
+
+ */
